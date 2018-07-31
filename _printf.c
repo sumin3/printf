dd10a85a59b9a978ebe6c5e7558079969efc55b1
sumin3 <suminyu@yahoo.com>
2018-07-31 18:48:41 +0000

    add length of buffer, use the length for write function. also add a if statment to check if specifier is match or not

--- _printf.c
+++ _printf.c
@@ -7,43 +7,53 @@
 #define BUFFER_SIZE 1024
 int _printf(const char *format, ...)
 {
-	va_list list;
-	int i = 0; /* counter */
-	int *index = NULL; /* index for buffer */
-	int index1 = 0;
-	char *buffer;
+        va_list list;
+        int i = 0; /* counter */
+        int *index = NULL; /* index for buffer */
+        int index1 = 0;
+        char *buffer;
+        int len = 0;
+        char *tmp;
 
-	index = &index1;
-	if (format == NULL)
-		return (-1);
-	/* initalize va list */
-	va_start(list, format);
-	/* malloc space for buffer */
-	buffer = malloc(BUFFER_SIZE);
-	if (buffer == NULL)
-		return (-1);
-	/* loop the string format until reach '\0' */
-	while (format && format[i] != '\0')
-	{
-		/* if find % symbol, pass the next character, list, index */
-		/* of buffer and buffer to get_sp function*/
-		if (format[i] == '%')
-		{
-			get_sp(format[i+1])(list, buffer, index);
-			/* increase i by 1 to skip the index % */
-			i++;
-		}
-		/* if % not found, just put character to buffer */
-		else
-		{
-			buffer[*index] = format[i];
-		}
-		i++;
-		(*index)++;
-	}
-	/* put a '\0' character at the end of the string */
-	buffer[*index] = '\0';
-	write(1, buffer, BUFFER_SIZE);
+        index = &index1;
+        if (format == NULL)
+                return (-1);
+        /* initalize va list */
+        va_start(list, format);
+        /* malloc space for buffer */
+        buffer = malloc(BUFFER_SIZE);
+        if (buffer == NULL)
+                return (-1);
+        /* loop the string format until reach '\0' */
+        while (format && format[i] != '\0')
+        {
+                /* if find % symbol, pass the next character, list, index */
+                /* of buffer and buffer to get_sp function*/
+                if (format[i] == '%')
+                {
+                        tmp = get_sp(format[i+1])(list, buffer, index);
+                        if (tmp == NULL)
+                        {
+                                buffer[*index] = '\0';
+                                free(buffer);
+                                return (-1);
+                        }
+                        /* increase i by 1 to skip the index % */
+                        i++;
+                }
+                /* if % not found, just put character to buffer */
+                else
+                {
+                        buffer[*index] = format[i];
+                }
+                i++;
+                (*index)++;
+        }
+        while (buffer[len] != '\0')
+                len++;
+        /* put a '\0' character at the end of the string */
+        buffer[*index] = '\0';
+	write(1, buffer, len);
 	free(buffer);
-	return (0);
+        return (len);
 }
