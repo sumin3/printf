d9d370fa07d96198dd438103a76b34974e61d523
sumin3 <suminyu@yahoo.com>
2018-07-31 23:57:37 +0000

    add prototype

--- holberton.h
+++ holberton.h
@@ -19,6 +19,7 @@ char *(*get_sp(char c))(va_list list, char *buffer, int *index);
 char *get_char(va_list list, char *buffer, int *index);
 char *get_string(va_list list, char *buffer, int *index);
 char *get_percent(va_list list, char *buffer, int *index);
+char *not_match(va_list list, char *buffer, int *index);
 int _putchar(char c);
-
+char *get_int(va_list list, char *buffer, int *index);
 #endif
