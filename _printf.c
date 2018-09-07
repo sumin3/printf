9550362cf4504555008d105e25b55a90b7200e04
sumin3 <suminyu@yahoo.com>
2018-09-07 09:02:41 +0000

    change logic for writing over 1024 characters. add a variable to hold the number of character that store in buffer ----- for case printing null character

--- _printf.c
+++ _printf.c
@@ -19,18 +19,17 @@ int _printf(const char *format, ...)
 	char *tmp_buffer = NULL;
 	int find_percent = 0, find = 0;
 	int not_match = 0;
-	int status = 0;
+	int status = 0, tmp_len = 0;
+	int char_count = 0;
 
 
 	if (format == NULL)
 		return (-1);
 	va_start(list, format);
-/* malloc space for buffer */
-
-/* loop the format until reach '\0' */
 	while (format && format[current_idx] != '\0')
 	{
 		find = 0;
+		char_count = 0;
 		lit_1st_idx = current_idx;
 		while (format[current_idx] != '%' && format[current_idx] != '\0')
 		{
@@ -43,7 +42,7 @@ int _printf(const char *format, ...)
 		{
 			if (format[current_idx + 1] != '\0')
 			{
-				tmp_buffer = get_sp(format[current_idx + 1])(list);
+				tmp_buffer = get_sp(format[current_idx + 1])(list, &char_count);
 				find_percent = 1;
 				if (tmp_buffer == NULL)
 				{
@@ -51,11 +50,11 @@ int _printf(const char *format, ...)
 					tmp_buffer[0] = '%';
 					tmp_buffer[1] = '\0';
 					not_match = 1;
+					char_count = 1;
 				}
 				else
 					current_idx++;
 			}
-			/* if next char is '\0' and has unpair specifier in format */
 			else if (format[current_idx + 1]
 				 == '\0' && (find_percent == 0 || not_match == 0))
 			{
@@ -69,46 +68,48 @@ int _printf(const char *format, ...)
 				tmp_buffer = malloc(sizeof(char) * 2);
 				tmp_buffer[0] = '%';
 				tmp_buffer[1] = '\0';
+				char_count = 1;
 			}
 
 		}
 		else
 		{
 			/* if % not found, just put literal char to tmp_buffer*/
-			tmp_buffer = malloc(current_idx - lit_1st_idx + 2);
+			tmp_buffer = malloc(current_idx - lit_1st_idx + 1);
 			if (tmp_buffer == NULL)
 				return (-1);
-			/* add '\0' at the end of string */
 			/* copy string */
 			num_to_tmp = current_idx - lit_1st_idx;
+			char_count = num_to_tmp;
 			for (i = 0; i < num_to_tmp; i++)
 			{
 				tmp_buffer[i] = format[lit_1st_idx];
 				lit_1st_idx++;
 			}
-
 			tmp_buffer[i] = '\0';
 
 		}
-		len = strlen(tmp_buffer);
-		for (j = 0; tmp_buffer[j] != '\0'; j = j + len)
+		len = char_count;
+		buffer = malloc(BUFFER_SIZE * sizeof(char));
+		for (j = 0; j < char_count; j = j + BUFFER_SIZE)
 		{
-		buffer = calloc(BUFFER_SIZE, sizeof(char));
-		if (buffer == NULL)
-			return (-1);
-		if (len > BUFFER_SIZE)
-		{
-			len = len - BUFFER_SIZE;
-			_strncpy(buffer, tmp_buffer, BUFFER_SIZE);
-			count += write(1, buffer, BUFFER_SIZE);
-		}
-		else
-		{
-			_strncpy(buffer, tmp_buffer, len);
-			count += write(1, buffer, len);
+
+			if (buffer == NULL)
+				return (-1);
+			if (len > BUFFER_SIZE)
+			{
+				len = len - BUFFER_SIZE;
+				_strncpy(buffer, tmp_buffer + j, BUFFER_SIZE);
+				count += write(1, buffer, BUFFER_SIZE);
+
+			}
+			else
+			{
+				_strncpy(buffer, tmp_buffer + j, BUFFER_SIZE);
+				count += write(1, buffer, len);
+			}
 		}
 		free(buffer);
-		}
 		free(tmp_buffer);
 		if (format[current_idx] != '\0' && find == 0)
 			current_idx++;
