9465f5543f8c54861b4e27e2dd5cd3a43f8cab9c
sumin3 <suminyu@yahoo.com>
2018-09-07 09:08:49 +0000

    change logic for writing over 1024 chars. also add a variable to hold the number of character that store in buffer

--- holberton.h
+++ holberton.h
@@ -11,17 +11,17 @@
 typedef struct sp
 {
 	char sp;
-	char *(*f)(va_list list);
+	char *(*f)(va_list list, int *char_count);
 } sp_t;
 
 int _printf(const char *format, ...);
-char *(*get_sp(char c))(va_list list);
-char *get_char(va_list list);
-char *get_string(va_list list);
-char *get_percent(va_list list);
-char *not_match(va_list list);
+char *(*get_sp(char c))(va_list list, int *char_count);
+char *get_char(va_list list, int *char_count);
+char *get_string(va_list list, int *char_count);
+char *get_percent(va_list list, int *char_count);
+char *not_match(va_list list, int *char_count);
 int _putchar(char c);
-char *get_int(va_list list);
+char *get_int(va_list list, int *char_count);
 char *_strncpy(char *dest, char *src, int n);
 int _strlen(char *s);
 #endif
--- main.c
+++ main.c
@@ -2,13 +2,10 @@
 #include <stdio.h>
 int main()
 {
-	char *str;
-	int count = 0, num = 23;
-	char a ='\0';
+	int n = 0;
 
-	str = "On February 6, 2018, SpaceX launched its Falcon Heavy rocket into space to much fanfare. Never one to misjudge the importance of well-timed publicity, SpaceX founder Elon Musk attached his cherry-red Tesla Roadster to the rocket and shot it into orbit. Images of the car, manned by an intrepid space-exploring mannequin dubbed Starman, were beamed around the world to delighted audiences";
-
-	count = _printf("num %d %c %s\n", num, a, str);
-	printf("count %d\n", count);
+	_printf("a%y%d%sb%", n, "hello");
+	/* n = _printf("hhhuvhhhhhhhhaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaxxxaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaannnnaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaavvvvvaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaawwwwwaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaeeaeeeeaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaappopopopopaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaartrtrtaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaayuyuuyuaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaacd");
+	   _printf("n =  %d\n", n);*/
 	return 0;
 }
